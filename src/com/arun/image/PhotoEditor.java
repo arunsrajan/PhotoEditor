43094e03146cb6abd7e3db0bbd92efe864be9062
arunsrajan <arunsrajan@gmail.com>
2015-03-11 17:38:27 +0530

    Copyright information on all files.

--- src/com/arun/image/PhotoEditor.java
+++ src/com/arun/image/PhotoEditor.java
@@ -1,5 +1,20 @@
 package com.arun.image;
+/*Copyright 2013 - 2015, Arun_Soundararajan (arun_srajan_2007@yahoo.com).and/or its affiliates.
 
+All files in this repository or distribution are licensed under the
+Apache License, Version 2.0 (the "License");
+you may not use any files in this repository or distribution except
+in compliance with the License.
+
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
